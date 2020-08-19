df039a8afe0cb89187ff37482b0aa178258eacd7
Alexander Orlovsky <javaprofi@gmx.de>
2020-08-19 12:58:37 +0200

    added some test for the registration component

--- jobmanager2020/src/app/components/userregistration/userregistration.component.ts
+++ jobmanager2020/src/app/components/userregistration/userregistration.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { first } from 'rxjs/operators';
+import { UserDetails } from 'src/app/shared/model/userdetails';
 
 @Component({
   templateUrl: 'userregistration.component.html',
@@ -35,11 +36,117 @@ export class UserregistrationComponent implements OnInit {
     return this.registerNewUserForm.controls;
   }
 
-  onSubmit() {
+  onSubmit(value: any): string {
+    let message: string;
+    message = "user wasn't registered";
+
+    let inputForFirstName: string;
+    let inputForLastName: string;
+    let inputForUserEmail: string;
+    let inputForUserCity: string;
+    let inputForBirthday: string;
+    let inputForCountry: string;
+    let inputForStreetName: string;
+    let inputForStreetNumber: string;
+
     this.submitted = true;
     if (this.registerNewUserForm.invalid) {
-      return;
+      return message;
+    } else {
+      inputForFirstName = value.firstName;
+      inputForLastName = value.lastName;
+      inputForUserEmail = value.userEmail;
+      inputForUserCity = value.userCity;
+      inputForBirthday = value.userBirthday;
+      inputForCountry = value.userCountry;
+      inputForStreetName = value.userStreetName;
+      inputForStreetNumber = value.userStreetNumber;
+
+      message = this.userProvidedInput(
+        inputForFirstName,
+        inputForLastName,
+        inputForUserEmail,
+        inputForUserCity,
+        inputForBirthday,
+        inputForCountry,
+        inputForStreetNumber,
+        inputForStreetName
+      );
+      if (message === 'new user should be created') {
+        message = 'new user was registered';
+        //create user details objects
+        //implement call for the persistence
+      }
     }
     this.loading = true;
+    return message;
+  }
+
+  userProvidedInput(
+    userfirstName: string,
+    userlastName: string,
+    userEmail: string,
+    userCity: string,
+    userBirthDay: string,
+    userCountryName: string,
+    userStreetNumber: string,
+    userStreetName: string
+  ): string {
+    let newUserData: UserDetails;
+    let createUser: boolean;
+    createUser = false;
+    let message: string;
+    message = 'user was not created';
+    let inputchain: string;
+    inputchain = userfirstName.concat(
+      ',' +
+        userlastName.toString() +
+        ',' +
+        userEmail.toString() +
+        ',' +
+        userCity.toString() +
+        ',' +
+        userBirthDay.toString() +
+        ',' +
+        userCountryName.toString() +
+        ',' +
+        userStreetNumber.toString() +
+        ',' +
+        userStreetName.toString()
+    );
+    let inputsArr = inputchain.split(',');
+
+    for (let i in inputsArr) {
+      if (this.checkIfProvidedInputNotValid(inputsArr[i]) === 'undefined') {
+        createUser = false;
+        break;
+      } else {
+        createUser = true;
+      }
+    }
+
+    message = this.checkIfUserAllowedToBeCreated(createUser);
+
+    return message;
+  }
+
+  checkIfProvidedInputNotValid(input: string): string {
+    let result: string;
+    result = 'undefined';
+
+    if (input.length > 0) {
+      result = input;
+    }
+
+    return result;
+  }
+
+  checkIfUserAllowedToBeCreated(userallowance: boolean): string {
+    let message: string;
+    message = 'user was not created';
+    if (userallowance === true) {
+      message = 'new user should be created';
+    }
+    return message;
   }
 }
--- jobmanager2020/src/test/componenttests/userregistration.component.spec.ts
+++ jobmanager2020/src/test/componenttests/userregistration.component.spec.ts
@@ -33,23 +33,265 @@ describe('UserregistrationComponent', () => {
     expect(userRegistrationComponent).toBeTruthy();
   });
 
-  // case user provided input for   userFirstName
-  // case user provided input for   userLastName
-  // case user provided input for   userEmail
-  // case user provided input for   userCityName
-  // case user provided input for   userBirthDay
-  // case user provided input for   userCountry
-  // case user provided input for   userStreetNumber
-  // case user provided input for   userStreetName
-  // case user provided input for   userEmploymentState
+  it('case: user input valid', () => {
+    let input: string = 'test';
+    expect(userRegistrationComponent.checkIfProvidedInputNotValid(input)).toBe(
+      'test'
+    );
+  });
 
-  // case user provided no input for   userFirstName
-  // case user provided no input for   userLastName
-  // case user provided no input for   userEmail
-  // case user provided no input for   userCityName
-  // case user provided no input for   userBirthDay
-  // case user provided no input for   userCountry
-  // case user provided no input for   userStreetNumber
-  // case user provided no input for   userStreetName
-  // case user provided no input for   userEmploymentState
+  it('case: user input invalid', () => {
+    let input: string = '';
+    expect(userRegistrationComponent.checkIfProvidedInputNotValid(input)).toBe(
+      'undefined'
+    );
+  });
+
+  it('case: user provided valid input for new registration', () => {
+    let userfirstName: string = 'John';
+    let userlastName: string = 'Smith';
+    let userEmail: string = 'johnsmith@testmail.com';
+    let userCity: string = 'New York';
+    let userBirthDay: string = '10.01.1966';
+    let userCountryName: string = 'USA';
+    let userStreetNumber: string = '22';
+    let userStreetName: string = 'Row street';
+
+    expect(
+      userRegistrationComponent.userProvidedInput(
+        userfirstName,
+        userlastName,
+        userEmail,
+        userCity,
+        userBirthDay,
+        userCountryName,
+        userStreetNumber,
+        userStreetName
+      )
+    ).toBe('new user should be created');
+  });
+
+  it('case: user provided invalid input no last name for new registration', () => {
+    let userfirstName: string = 'John';
+    let userlastName: string = '';
+    let userEmail: string = 'johnsmith@testmail.com';
+    let userCity: string = 'New York';
+    let userBirthDay: string = '10.01.1966';
+    let userCountryName: string = 'USA';
+    let userStreetNumber: string = '22';
+    let userStreetName: string = 'Row street';
+
+    expect(
+      userRegistrationComponent.userProvidedInput(
+        userfirstName,
+        userlastName,
+        userEmail,
+        userCity,
+        userBirthDay,
+        userCountryName,
+        userStreetNumber,
+        userStreetName
+      )
+    ).toBe('user was not created');
+  });
+
+  it('case: user provided invalid input no first name for new registration', () => {
+    let userfirstName: string = '';
+    let userlastName: string = 'dddd';
+    let userEmail: string = 'johnsmith@testmail.com';
+    let userCity: string = 'New York';
+    let userBirthDay: string = '10.01.1966';
+    let userCountryName: string = 'USA';
+    let userStreetNumber: string = '22';
+    let userStreetName: string = 'Row street';
+
+    expect(
+      userRegistrationComponent.userProvidedInput(
+        userfirstName,
+        userlastName,
+        userEmail,
+        userCity,
+        userBirthDay,
+        userCountryName,
+        userStreetNumber,
+        userStreetName
+      )
+    ).toBe('user was not created');
+  });
+
+  it('case: user provided invalid input no email for new registration', () => {
+    let userfirstName: string = 'dddd';
+    let userlastName: string = 'dddd';
+    let userEmail: string = '';
+    let userCity: string = 'New York';
+    let userBirthDay: string = '10.01.1966';
+    let userCountryName: string = 'USA';
+    let userStreetNumber: string = '22';
+    let userStreetName: string = 'Row street';
+
+    expect(
+      userRegistrationComponent.userProvidedInput(
+        userfirstName,
+        userlastName,
+        userEmail,
+        userCity,
+        userBirthDay,
+        userCountryName,
+        userStreetNumber,
+        userStreetName
+      )
+    ).toBe('user was not created');
+  });
+
+  it('case: user provided invalid input no birthday for new registration', () => {
+    let userfirstName: string = 'dddd';
+    let userlastName: string = 'dddd';
+    let userEmail: string = 'test@yes.com';
+    let userCity: string = 'New York';
+    let userBirthDay: string = '';
+    let userCountryName: string = 'USA';
+    let userStreetNumber: string = '22';
+    let userStreetName: string = 'Row street';
+
+    expect(
+      userRegistrationComponent.userProvidedInput(
+        userfirstName,
+        userlastName,
+        userEmail,
+        userCity,
+        userBirthDay,
+        userCountryName,
+        userStreetNumber,
+        userStreetName
+      )
+    ).toBe('user was not created');
+  });
+
+  it('case: user provided invalid input no country for new registration', () => {
+    let userfirstName: string = 'dddd';
+    let userlastName: string = 'dddd';
+    let userEmail: string = 'test@yes.com';
+    let userCity: string = 'New York';
+    let userBirthDay: string = '12.01.2039';
+    let userCountryName: string = '';
+    let userStreetNumber: string = '22';
+    let userStreetName: string = 'Row street';
+
+    expect(
+      userRegistrationComponent.userProvidedInput(
+        userfirstName,
+        userlastName,
+        userEmail,
+        userCity,
+        userBirthDay,
+        userCountryName,
+        userStreetNumber,
+        userStreetName
+      )
+    ).toBe('user was not created');
+  });
+
+  it('case: user provided invalid input no stree number for new registration', () => {
+    let userfirstName: string = 'dddd';
+    let userlastName: string = 'dddd';
+    let userEmail: string = 'test@yes.com';
+    let userCity: string = 'New York';
+    let userBirthDay: string = '12.01.2039';
+    let userCountryName: string = 'ddd';
+    let userStreetNumber: string = '';
+    let userStreetName: string = 'Row street';
+
+    expect(
+      userRegistrationComponent.userProvidedInput(
+        userfirstName,
+        userlastName,
+        userEmail,
+        userCity,
+        userBirthDay,
+        userCountryName,
+        userStreetNumber,
+        userStreetName
+      )
+    ).toBe('user was not created');
+  });
+
+  it('case: user provided invalid input no stree name for new registration', () => {
+    let userfirstName: string = 'dddd';
+    let userlastName: string = 'dddd';
+    let userEmail: string = 'test@yes.com';
+    let userCity: string = 'New York';
+    let userBirthDay: string = '12.01.2039';
+    let userCountryName: string = 'ddd';
+    let userStreetNumber: string = 'ddd';
+    let userStreetName: string = '';
+
+    expect(
+      userRegistrationComponent.userProvidedInput(
+        userfirstName,
+        userlastName,
+        userEmail,
+        userCity,
+
+        userBirthDay,
+        userCountryName,
+        userStreetNumber,
+        userStreetName
+      )
+    ).toBe('user was not created');
+  });
+
+  it('case: user provided invalid input no city name for new registration', () => {
+    let userfirstName: string = 'dddd';
+    let userlastName: string = 'dddd';
+    let userEmail: string = 'test@yes.com';
+    let userCity: string = '';
+    let userBirthDay: string = '12.01.2039';
+    let userCountryName: string = 'ddd';
+    let userStreetNumber: string = 'ddd';
+    let userStreetName: string = 'cool';
+
+    expect(
+      userRegistrationComponent.userProvidedInput(
+        userfirstName,
+        userlastName,
+        userEmail,
+        userCity,
+
+        userBirthDay,
+        userCountryName,
+        userStreetNumber,
+        userStreetName
+      )
+    ).toBe('user was not created');
+  });
+
+  it('case: user provided invalid input for all fields for new registration', () => {
+    let userfirstName: string = '';
+    let userlastName: string = '';
+    let userEmail: string = '';
+    let userCity: string = 'New York';
+    let userBirthDay: string = '';
+    let userCountryName: string = '';
+    let userStreetNumber: string = '';
+    let userStreetName: string = '';
+
+    expect(
+      userRegistrationComponent.userProvidedInput(
+        userfirstName,
+        userlastName,
+        userEmail,
+        userCity,
+        userBirthDay,
+        userCountryName,
+        userStreetNumber,
+        userStreetName
+      )
+    ).toBe('user was not created');
+  });
+
+  it('case: when onSubmit called without any valid input data', () => {
+    expect(userRegistrationComponent.onSubmit('')).toBe(
+      "user wasn't registered"
+    );
+  });
 });
