d2175af5f7466f5ea98ada7bdb5c6918086ef2f1
Alexander Orlovsky <javaprofi@gmx.de>
2020-12-15 21:35:29 +0100

    added test case if a actual file was provided!

--- jobmanager2020/src/test/servicetests/email.service.spec.ts
+++ jobmanager2020/src/test/servicetests/email.service.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { EmailService } from '../../app/services/email.service';
-
+import fs from 'fs';
 describe('EmailService', () => {
   let service: EmailService;
   let emailtopic: string;
@@ -32,5 +32,28 @@ describe('EmailService', () => {
 
   it('case: validate that provided file is not a valid file', () => {
     expect(service.validateUserPdfFile(userfile)).toBeFalsy();
+  });
+
+  it('case: validate that provided file is a file', () => {
+    try {
+      const data = fs.readFileSync('src/test/testassets/testdocument.pdf');
+
+      /*
+     This code only need for manual verfifcation
+    fs.writeFile('src/test/testassets/res.pdf', data, (err) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+      });*/
+
+      userfile = new File([data], 'testdocument.pdf', {
+        type: 'application/pdf',
+      });
+
+      expect(service.validateUserPdfFile(userfile)).toBeTruthy();
+    } catch (err) {
+      console.error(err);
+    }
   });
 });
