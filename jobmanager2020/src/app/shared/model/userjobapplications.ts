066a02b35aea905b94d5c8d9ec6f4a06874458c0
Alexander Orlovsky <javaprofi@gmx.de>
2020-12-16 16:35:07 +0100

    added test case where EmailDetails can be retrieved

--- jobmanager2020/src/app/shared/model/userjobapplications.ts
+++ jobmanager2020/src/app/shared/model/userjobapplications.ts
@@ -1,5 +1,8 @@
+import { UserEmailDetails } from './useremaildetails';
+
 export class UserJobApplications {
   private userEmailLogin: string = 'undefined';
+  private inMemorySetOfUserEmailDetails = new Set<UserEmailDetails>();
 
   public constructor(userInputForEmailLogin: string) {
     this.userEmailLogin = userInputForEmailLogin;
@@ -7,5 +10,36 @@ export class UserJobApplications {
 
   public getUserEmailLogin(): string {
     return this.userEmailLogin;
+  }
+
+  public addAndCountEmailDetailsToUserJobApplication(
+    userInputForEmailDetails: UserEmailDetails
+  ): number {
+    this.getinMemorySetOfUserEmailDetails().add(userInputForEmailDetails);
+    return this.getinMemorySetOfUserEmailDetails().size;
+  }
+
+  public getinMemorySetOfUserEmailDetails(): Set<UserEmailDetails> {
+    return this.inMemorySetOfUserEmailDetails;
+  }
+
+  public findUserEmailDetailsByEmailDetails(
+    userInputForEmailDetails
+  ): UserEmailDetails {
+    let emptyFile: File;
+    let resultOfSearch: UserEmailDetails = new UserEmailDetails(
+      'undefined',
+      'undefined',
+      emptyFile
+    );
+
+    for (const itemUserEmailDetails of this.getinMemorySetOfUserEmailDetails().values()) {
+      if (userInputForEmailDetails === itemUserEmailDetails) {
+        resultOfSearch = itemUserEmailDetails;
+        break;
+      }
+    }
+
+    return resultOfSearch;
   }
 }
--- jobmanager2020/src/test/modeltests/userjobapplications.spec.ts
+++ jobmanager2020/src/test/modeltests/userjobapplications.spec.ts
@@ -1,12 +1,34 @@
 import { UserJobApplications } from '../../app/shared/model/userjobapplications';
+import { UserEmailDetails } from '../../app/shared/model/useremaildetails';
+import fs from 'fs';
 
 describe('UserJobApplications', () => {
   let userJobApplications: UserJobApplications = null;
   let userEmailLogin: string;
+  let userEmailDetails: UserEmailDetails;
+  let userEmailTopic: string;
+  let userEmailBodyText: string;
+  let userEmailPdfFile: File;
 
   beforeEach(() => {
     userEmailLogin = 'johnsmith@test.com';
     userJobApplications = new UserJobApplications(userEmailLogin);
+
+    try {
+      userEmailTopic = 'Applications as software developer';
+      userEmailBodyText = 'this is a simple text';
+      const data = fs.readFileSync('src/test/testassets/testdocument.pdf');
+      userEmailPdfFile = new File([data], 'testdocument.pdf', {
+        type: 'application/pdf',
+      });
+      userEmailDetails = new UserEmailDetails(
+        userEmailTopic,
+        userEmailBodyText,
+        userEmailPdfFile
+      );
+    } catch (err) {
+      console.error(err);
+    }
   });
 
   test('UserJobApplications are defined', () => {
@@ -15,6 +37,23 @@ describe('UserJobApplications', () => {
 
   test('case: User provided valid email login', () => {
     expect(userJobApplications.getUserEmailLogin()).toBe('johnsmith@test.com');
+  });
+
+  test('case: User created a email details with email topic, bodytext, pdf file check if it was save in job applications', () => {
+    expect(
+      userJobApplications.addAndCountEmailDetailsToUserJobApplication(
+        userEmailDetails
+      )
+    ).toBe(1);
+  });
+
+  test('case: User created a email details with email topic, bodytext, pdf file check the after it was added to job applications you can retrieve the email details', () => {
+    userJobApplications.addAndCountEmailDetailsToUserJobApplication(
+      userEmailDetails
+    );
+    expect(
+      userJobApplications.findUserEmailDetailsByEmailDetails(userEmailDetails)
+    ).toBe(userEmailDetails);
   });
 
   afterEach(() => {
