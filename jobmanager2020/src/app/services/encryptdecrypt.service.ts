304b933b4695409fcdf1c7dcea6ed257af75d939
Alexander Orlovsky <javaprofi@gmx.de>
2021-01-07 12:43:35 +0100

    added simple encryption service for encryting and decrypting data

--- jobmanager2020/src/app/services/encryptdecrypt.service.ts
+++ jobmanager2020/src/app/services/encryptdecrypt.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { debug } from 'console';
+import SecureLS from 'secure-ls';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EncryptdecryptService {
+  constructor() {}
 
-  constructor() { }
+  public encryptUserData(
+    inputForKey: string,
+    inputForUserData: string
+  ): SecureLS {
+    let result: string = 'undefined';
+    let secStorage = new SecureLS({ encodingType: 'aes' });
+
+    if (inputForKey.length >= 12 && inputForUserData.length >= 2) {
+      secStorage.set(inputForKey, { data: inputForUserData });
+    }
+
+    return secStorage;
+  }
+
+  public decryptUserData(inputForkey: string, encryptedData: SecureLS): string {
+    let result: string = 'undefined';
+    if (
+      encryptedData != null &&
+      encryptedData != undefined &&
+      inputForkey.length >= 12
+    ) {
+      result = encryptedData.get(inputForkey).data;
+    }
+    return result;
+  }
 }
--- jobmanager2020/src/test/servicetests/encryptdecrypt.service.spec.ts
+++ jobmanager2020/src/test/servicetests/encryptdecrypt.service.spec.ts
@@ -1,5 +1,5 @@
 import { TestBed } from '@angular/core/testing';
-
+import SecureLS from 'secure-ls';
 import { EncryptdecryptService } from '../../app/services/encryptdecrypt.service';
 
 describe('EncryptdecryptService', () => {
@@ -12,5 +12,23 @@ describe('EncryptdecryptService', () => {
 
   it('EncryptdecryptService can be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('Encrypt data by given key', () => {
+    let key: string = 'SuperSecureKey';
+    let userdata: string = 'veryprivateuserdata';
+    //Get encrypted data!
+    expect(
+      service.encryptUserData(key, userdata) != null &&
+        service.encryptUserData(key, userdata) != undefined
+    ).toBeTruthy();
+  });
+
+  it('Decrypt data by given key', () => {
+    let key: string = 'SuperSecureKey';
+    let userdata: string = 'veryprivateuserdata';
+    let encryptedData = service.encryptUserData(key, userdata);
+
+    expect(service.decryptUserData(key, encryptedData)).toEqual(userdata);
   });
 });
