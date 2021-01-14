19d93cd43d463fd89e6d0d9cc216e9c100757d74
Alexander Orlovsky <javaprofi@gmx.de>
2021-01-14 19:27:03 +0100

    add case when admin user must be registered

--- jobmanager2020/src/app/shared/model/adminuser.ts
+++ jobmanager2020/src/app/shared/model/adminuser.ts
@@ -1,1 +1,40 @@
-export class AdminUserForLogin {}
+import { AdminUserAffiliation } from './adminuseraffiliation.enum';
+
+export class AdminUserForLogin {
+  private adminFirstName: string;
+  private adminLastName: string;
+  private adminEmail: string;
+  private adminActivated: boolean = false;
+  private adminOrga: AdminUserAffiliation;
+  private adminPassword: string;
+
+  constructor(
+    inputForFirstName: string,
+    inputForLastName: string,
+    inputForEmail: string,
+    inputForAdminUserActivation: boolean,
+    inputTypeOfOrga: AdminUserAffiliation,
+    inputForProtectedPassword: string
+  ) {
+    this.adminFirstName = inputForFirstName;
+    this.adminLastName = inputForLastName;
+    this.adminEmail = inputForEmail;
+    this.adminActivated = inputForAdminUserActivation;
+    this.adminOrga = inputTypeOfOrga;
+    this.adminPassword = inputForProtectedPassword;
+    /*console.log(
+      'following admin created:\n' +
+        this.adminFirstName +
+        ' ' +
+        this.adminLastName +
+        ' ' +
+        this.adminEmail +
+        ' activated: ' +
+        this.adminActivated +
+        ' ' +
+        this.adminOrga +
+        ' with password: ' +
+        this.adminPassword
+    );*/
+  }
+}
--- jobmanager2020/src/test/modeltests/adminuser.spec.ts
+++ jobmanager2020/src/test/modeltests/adminuser.spec.ts
@@ -1,10 +1,17 @@
 import { AdminUserForLogin } from '../../app/shared/model/adminuser';
-
+import { AdminUserAffiliation } from '../../app/shared/model/adminuseraffiliation.enum';
 describe('Admin user behavior', () => {
   let adminUser: AdminUserForLogin = null;
 
   beforeEach(() => {
-    adminUser = new AdminUserForLogin();
+    adminUser = new AdminUserForLogin(
+      'John',
+      'Smith',
+      'tester@tester.com',
+      true,
+      AdminUserAffiliation.NPO,
+      'testpassword0000*-'
+    );
   });
 
   test('Admin user is defined', () => {
