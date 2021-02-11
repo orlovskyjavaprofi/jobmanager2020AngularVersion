21799b213dcb23d11a78730bacfcde6ad2cff88d
Alexander Orlovsky <javaprofi@gmx.de>
2021-02-11 18:45:32 +0100

    add the debug output for a set smtp settings form

--- jobmanager2020/src/app/components/savsmtpmailsettings/savsmtpmailsettings.component.ts
+++ jobmanager2020/src/app/components/savsmtpmailsettings/savsmtpmailsettings.component.ts
@@ -1,8 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { SmtpPorts } from 'src/app/shared/model/smtpports.enum';
 interface contractForSmtpPortsSelectionRepresentation {
   value: number;
+  viewValue: string;
+}
+
+interface contractForUsageOfSecureSslTlsPort {
+  value: string;
   viewValue: string;
 }
 
@@ -13,8 +18,31 @@ interface contractForSmtpPortsSelectionRepresentation {
 })
 export class SavsmtpmailsettingsComponent implements OnInit {
   constructor() {}
+
+  UserChkBoxChekedForGDPR: boolean = false;
+
   smtpSettingSaveForm = new FormGroup({
-    selSmtpEmailFormControl: new FormControl(''),
+    selSmtpPortFormControl: new FormControl(''),
+    selUserSelectionForSecureSSLTLSFormControl: new FormControl(''),
+    chkBoxUserConfirmReadGDPRFormControl: new FormControl('', [
+      Validators.required,
+      Validators.requiredTrue,
+    ]),
+    smtpHostFormControl: new FormControl('', [
+      Validators.required,
+      Validators.maxLength(20),
+      Validators.minLength(4),
+    ]),
+    smtpUserNameFormControl: new FormControl('', [
+      Validators.required,
+      Validators.maxLength(20),
+      Validators.minLength(4),
+    ]),
+    smtpUserPasswordFormControl: new FormControl('', [
+      Validators.required,
+      Validators.maxLength(20),
+      Validators.minLength(4),
+    ]),
   });
 
   selectionViewForDifferentSmtpEmailPorts: contractForSmtpPortsSelectionRepresentation[] = [
@@ -40,5 +68,58 @@ export class SavsmtpmailsettingsComponent implements OnInit {
     },
   ];
 
+  selectionViewForUsageOfSecureSslTlsPort: contractForUsageOfSecureSslTlsPort[] = [
+    {
+      value: 'Yes',
+      viewValue: 'Yes',
+    },
+    {
+      value: 'No',
+      viewValue: 'No',
+    },
+  ];
+
   ngOnInit(): void {}
+
+  onFormSubmit(): void {
+    this.debutOutput();
+  }
+
+  public resetsmtpSettingSaveForm(): void {
+    this.smtpSettingSaveForm.reset({
+      selSmtpPortFormControl: '',
+      selUserSelectionForSecureSSLTLSFormControl: '',
+      chkBoxUserConfirmReadGDPRFormControl: '',
+      smtpHostFormControl: '',
+      smtpUserNameFormControl: '',
+      smtpUserPasswordFormControl: '',
+    });
+  }
+
+  private debutOutput(): void {
+    console.log(
+      'Email server host: ' + this.smtpSettingSaveForm.value.smtpHostFormControl
+    );
+    console.log(
+      'Email server port: ' +
+        this.smtpSettingSaveForm.value.selSmtpPortFormControl
+    );
+    console.log(
+      'SMTP user name: ' +
+        this.smtpSettingSaveForm.value.smtpUserNameFormControl
+    );
+    console.log(
+      'SMTP user password: ' +
+        this.smtpSettingSaveForm.value.smtpUserNameFormControl
+    );
+    console.log(
+      'Port 456 selected 2nd question ' +
+        this.smtpSettingSaveForm.value
+          .selUserSelectionForSecureSSLTLSFormControl
+    );
+    console.log(
+      'Checkbox for GDPR checked? ' +
+        this.smtpSettingSaveForm.value.chkBoxUserConfirmReadGDPRFormControl
+    );
+  }
 }
