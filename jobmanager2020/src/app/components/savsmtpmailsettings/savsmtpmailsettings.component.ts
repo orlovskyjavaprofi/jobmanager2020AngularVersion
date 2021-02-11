7829892fb3f9682d975188da563bda254dbb5641
Alexander Orlovsky <javaprofi@gmx.de>
2021-02-11 16:47:32 +0100

    added selection box for smt ports

--- jobmanager2020/src/app/components/savsmtpmailsettings/savsmtpmailsettings.component.ts
+++ jobmanager2020/src/app/components/savsmtpmailsettings/savsmtpmailsettings.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { SmtpPorts } from 'src/app/shared/model/smtpports.enum';
+interface contractForSmtpPortsSelectionRepresentation {
+  value: number;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-savsmtpmailsettings',
   templateUrl: './savsmtpmailsettings.component.html',
-  styleUrls: ['./savsmtpmailsettings.component.css']
+  styleUrls: ['./savsmtpmailsettings.component.css'],
 })
 export class SavsmtpmailsettingsComponent implements OnInit {
+  constructor() {}
+  smtpSettingSaveForm = new FormGroup({
+    selSmtpEmailFormControl: new FormControl(''),
+  });
 
-  constructor() { }
+  selectionViewForDifferentSmtpEmailPorts: contractForSmtpPortsSelectionRepresentation[] = [
+    {
+      value: SmtpPorts.StandardMtaPort,
+      viewValue: SmtpPorts.StandardMtaPort.toString(),
+    },
+    {
+      value: SmtpPorts.SslTlsPort,
+      viewValue: SmtpPorts.SslTlsPort.toString(),
+    },
+    {
+      value: SmtpPorts.SslTlsAlternativePort,
+      viewValue: SmtpPorts.SslTlsAlternativePort.toString(),
+    },
+    {
+      value: SmtpPorts.SslPort,
+      viewValue: SmtpPorts.SslPort.toString(),
+    },
+    {
+      value: SmtpPorts.MsaPort,
+      viewValue: SmtpPorts.MsaPort.toString(),
+    },
+  ];
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
---
+++ jobmanager2020/src/app/shared/model/smtpports.enum.ts
@@ -0,0 +1,7 @@
+export enum SmtpPorts {
+  StandardMtaPort = 25,
+  SslTlsPort = 465,
+  SslTlsAlternativePort = 2525,
+  MsaPort = 587,
+  SslPort = 995,
+}
