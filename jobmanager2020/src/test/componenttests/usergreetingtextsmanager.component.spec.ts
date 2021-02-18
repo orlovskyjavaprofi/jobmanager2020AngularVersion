b6844e28c1bfbb80d7a37d9b4c47aefdf3f5591e
Alexander Orlovsky <javaprofi@gmx.de>
2021-02-18 10:31:33 +0100

    added the greeting text template

--- jobmanager2020/src/test/componenttests/usergreetingtextsmanager.component.spec.ts
+++ jobmanager2020/src/test/componenttests/usergreetingtextsmanager.component.spec.ts
@@ -1,4 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { UsergreetingtextsmanagerComponent } from '../../app/components/usergreetingtextsmanager/usergreetingtextsmanager.component';
 
@@ -9,6 +16,17 @@ describe('UsergreetingtextsmanagerComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [UsergreetingtextsmanagerComponent],
+      imports: [
+        ReactiveFormsModule,
+        RouterTestingModule,
+        FormsModule,
+        BrowserAnimationsModule,
+        ReactiveFormsModule,
+        MatCheckboxModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatSelectModule,
+      ],
     }).compileComponents();
   });
 
